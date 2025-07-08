1c0b26c227ad5b26d873346211f786cb4be7facf
Hannah Johnson <hannah.johnson@ocado.com>
2025-07-08 16:59:29 +0100

    Initial web scraping / crawling

--- src/app.py
+++ src/app.py
@@ -3,6 +3,9 @@ import logging
 import os
 import boto3
 
+from src.utils import load_yaml
+from src.web_crawling import extract_relevant_events_from_bill_murray_homepage
+
 # Configure logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -15,30 +18,41 @@ def lambda_handler(event, context):
     Lambda function that sends 'Hello World' via SMS
     """
     try:
-        # Get the SNS topic ARN from environment variables
-        topic_arn = os.environ['SNS_TOPIC_ARN']
-        
-        # Publish message to SNS topic
-        response = sns.publish(
-            TopicArn=topic_arn,
-            Message='Hello World',
-            MessageAttributes={
-                'AWS.SNS.SMS.SenderID': {
-                    'DataType': 'String',
-                    'StringValue': 'HelloApp'
-                },
-                'AWS.SNS.SMS.SMSType': {
-                    'DataType': 'String',
-                    'StringValue': 'Transactional'
+        # Logic for web scraping here. Output is an array of urls for websites to check
+        comedians = load_yaml('comedians.yaml')['comedians']
+        comedians_and_urls = extract_relevant_events_from_bill_murray_homepage(comedians)
+        if len(comedians_and_urls) > 0:
+            # Get the SNS topic ARN from environment variables
+            topic_arn = os.environ['SNS_TOPIC_ARN']
+
+            # Publish message to SNS topic
+            response = sns.publish(
+                TopicArn=topic_arn,
+                Message='Hello World',
+                MessageAttributes={
+                    'AWS.SNS.SMS.SenderID': {
+                        'DataType': 'String',
+                        'StringValue': 'HelloApp'
+                    },
+                    'AWS.SNS.SMS.SMSType': {
+                        'DataType': 'String',
+                        'StringValue': 'Transactional'
+                    }
                 }
+            )
+
+            logger.info(f"Message sent successfully: {response['MessageId']}")
+            return {
+                'statusCode': 200,
+                'body': json.dumps('SMS sent successfully.')
             }
-        )
-        
-        logger.info(f"Message sent successfully: {response['MessageId']}")
-        return {
-            'statusCode': 200,
-            'body': json.dumps('SMS sent successfully')
-        }
+
+        else:
+            return {
+                'statusCode': 200,
+                'body': json.dumps('No SMS sent.')
+            }
+
     except Exception as e:
         logger.error(f"Error sending SMS: {str(e)}")
         return {
